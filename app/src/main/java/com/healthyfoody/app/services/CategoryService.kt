19cbc5f773326e570f0b8b39bee7c3a498eb263b
Diego B <u201616874@upc.edu.pe>
2020-05-08 23:55:04 -0500

    carga de categorias mediante llamados al api

--- app/src/main/java/com/healthyfoody/app/services/CategoryService.kt
+++ app/src/main/java/com/healthyfoody/app/services/CategoryService.kt
@@ -1,13 +1,16 @@
 package com.healthyfoody.app.services
 
 import com.healthyfoody.app.models.Category
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
 
-class CategoryService() {
-    val categories : List<Category> = listOf(Category("1","Categoria 2","categoria_tortilla"),
-        Category("3","Categoria 3","categoria_pasta"),Category("4","Categoria 4","categoria_menestra"),
-        Category("5","Categoria 5","categoria_ensalada"))
-    fun findAll():List<Category>{
-        return categories
-    }
+interface CategoryService {
+
+    @GET("menu/categories")
+    fun findAll(): Call<List<Category>>
+
+    @GET("menu/categories/{id}")
+    fun findCategory(@Path("id") id: String): Call<Category>
 
 }
